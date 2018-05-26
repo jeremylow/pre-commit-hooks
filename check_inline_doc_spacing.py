37a77e39ded767e0a554ab58f7485e90f0818e35
Jeremy Low <jeremylow@gmail.com>
2018-05-26 17:33:26 -0400

    better lineno identification

--- check_inline_doc_spacing.py
+++ check_inline_doc_spacing.py
@@ -10,35 +10,38 @@ import sys
 
 
 def check_indents(files):
+    retval = 0
     failures = []
-    finder = re.compile(r'(?:\n{1,})([ ]{1,})(?:.*)', re.UNICODE)
+    finder = re.compile(r'^([ ]{1,})(?:.*)', re.UNICODE)
 
     for f in files:
         with open(f) as _file:
             source = _file.read()
         nodes = ast.parse(source).body
-        doc_strings = [
-            (ast.get_docstring(node), node)
-            for node in nodes
-            if isinstance(node, ast.FunctionDef)
-        ]
-        for (dstring, node) in doc_strings:
-            if not dstring: continue
+        for node in nodes:
+            try:
+                doc_string = ast.get_docstring(node)
+            except:
+                continue
+            lineno = node.lineno + 1
 
-            for line in dstring.split('\n'):
+            if not doc_string: continue
+
+            for line in doc_string.split('\n'):
                 white_space_strings = re.findall(pattern=finder, string=line)
-                lineno = node.lineno
-                if 0 < len(line) < 4:
-                    failures.append("{0}:{1} in {2}: spacing not a multiple of four.".format(f, node.lineno, node.name))
+                if not white_space_strings:
+                    continue
+                if 0 < len(white_space_strings[0]) < 4:
+                    failures.append("{0}:{1} in {2}: spacing not a multiple of four.".format(f, lineno, node.name))
                     retval = 1
-                elif len(line) % 4 != 0:
-                    failures.append("{0}:{1} in {2}: spacing not a multiple of four.".format(f, node.lineno, node.name))
+                elif len(white_space_strings[0]) % 4 != 0:
+                    failures.append("{0}:{1} in {2}: spacing not a multiple of four.".format(f, lineno, node.name))
                     retval = 1
                 lineno += 1
     if retval:
         [print(failure) for failure in failures]
         return retval
-    return 0
+    return retval
 
 
 if __name__ == "__main__":
